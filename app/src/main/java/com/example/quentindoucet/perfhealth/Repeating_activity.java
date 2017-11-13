3a47a8331f89d4011c13bb46f5ea3c524d5825d9
MajidSrh <33104552+MajidSrh@users.noreply.github.com>
2017-11-13 20:26:06 +0100

    Notifications OK.

---
+++ app/src/main/java/com/example/quentindoucet/perfhealth/Repeating_activity.java
@@ -0,0 +1,19 @@
+package com.example.quentindoucet.perfhealth;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by majid on 13/11/2017.
+ */
+
+public class Repeating_activity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.repeating_activity_layout);
+    }
+}
