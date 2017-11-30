2cbe436ddf25fb4c7cdf89dbd758579603255836
AlexisBerger <alexis-berger@orange.fr>
2017-11-30 12:44:33 -0500

    notif place

--- app/src/main/java/com/example/quentindoucet/perfhealth/service/PlaceService.java
+++ app/src/main/java/com/example/quentindoucet/perfhealth/service/PlaceService.java
@@ -1,29 +1,43 @@
 package com.example.quentindoucet.perfhealth.service;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.quentindoucet.perfhealth.R;
 import com.example.quentindoucet.perfhealth.controler.PlacesManager;
 import com.example.quentindoucet.perfhealth.model.Place;
 import com.example.quentindoucet.perfhealth.model.Places;
+import com.example.quentindoucet.perfhealth.view.MainActivity;
 
 import java.util.Iterator;
 
 public class PlaceService extends Service {
+
     private static final String TAG = "BOOMBOOMTESTGPS";
     private static final int LOCATION_INTERVAL = 1000;
     private static final float LOCATION_DISTANCE = 10f;
+    private static final int mNotificationId = 123;
+
+    private Places lastLocation = null;
+    private NotificationManager mNotificationManager;
+    private NotificationCompat.Builder mBuilder;
+
     LocationListener[] mLocationListeners = new LocationListener[]{
             new LocationListener(LocationManager.GPS_PROVIDER),
             new LocationListener(LocationManager.NETWORK_PROVIDER)
     };
+
     private LocationManager mLocationManager = null;
 
     public PlaceService() {
@@ -44,6 +58,36 @@ public class PlaceService extends Service {
     @Override
     public void onCreate() {
         Log.e(TAG, "onCreate");
+
+        // The id of the channel.
+        String CHANNEL_ID = "my_channel_01";
+        mBuilder =
+                new NotificationCompat.Builder(this, CHANNEL_ID)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+// Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your app to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+// Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+// Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
         initializeLocationManager();
         try {
             mLocationManager.requestLocationUpdates(
@@ -70,9 +114,9 @@ public class PlaceService extends Service {
         Log.e(TAG, "onDestroy");
         super.onDestroy();
         if (mLocationManager != null) {
-            for (int i = 0; i < mLocationListeners.length; i++) {
+            for (LocationListener mLocationListener : mLocationListeners) {
                 try {
-                    mLocationManager.removeUpdates(mLocationListeners[i]);
+                    mLocationManager.removeUpdates(mLocationListener);
                 } catch (Exception ex) {
                     Log.i(TAG, "fail to remove location listners, ignore", ex);
                 }
@@ -102,11 +146,24 @@ public class PlaceService extends Service {
         public void onLocationChanged(Location location) {
             Log.e(TAG, "onLocationChanged: " + location);
             Places places = placesManager.compare(location);
-            for (Iterator<Place> i = places.iterator(); i.hasNext(); ) {
-                Place place = i.next();
-                Toast.makeText(PlaceService.this, place.getName() + " : " + place.getAction(), Toast.LENGTH_LONG).show();
-            }
 
+            if (lastLocation == null)
+                lastLocation = places;
+            else
+                places.removeAll(lastLocation);
+
+            for (Place place : places) {
+
+                // mNotificationId is a unique integer your app uses to identify the
+                // notification. For example, to cancel the notification, you can pass its ID
+                // number to NotificationManager.cancel().
+                mBuilder.setContentTitle(place.getName())
+                        .setContentText(place.getAction());
+                mNotificationManager.notify(mNotificationId, mBuilder.build());
+
+                //Toast.makeText(PlaceService.this, place.getName() + " : " + place.getAction(), Toast.LENGTH_LONG).show();
+            }
+            lastLocation = places;
             mLastLocation.set(location);
         }
 
