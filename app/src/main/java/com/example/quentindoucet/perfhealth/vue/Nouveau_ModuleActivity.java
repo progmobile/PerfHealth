f5268e27338eaec9afc9e1c46ecb39b04fa34c83
benjamin <benjamin.boussarie.rr@gmail.com>
2017-12-04 15:14:47 -0500

    v2.2 benjamin

--- app/src/main/java/com/example/quentindoucet/perfhealth/vue/Nouveau_ModuleActivity.java
+++ app/src/main/java/com/example/quentindoucet/perfhealth/vue/Nouveau_ModuleActivity.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.NumberPicker;
 
 import com.example.quentindoucet.perfhealth.R;
 import com.example.quentindoucet.perfhealth.controleur.Nouveau_ModuleManager;
@@ -15,25 +14,22 @@ public class Nouveau_ModuleActivity extends AppCompatActivity {
 private int frequence;
 private Nouveau_ModuleManager nouveau_moduleManager;
 private EditText eT;
+private EditText eN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         nouveau_moduleManager = new Nouveau_ModuleManager();
         setContentView(R.layout.activity_nouveau_module);
         eT = findViewById(R.id.nommodule);
-        final NumberPicker numberPicker = (NumberPicker) findViewById(R.id.numberPicker);
+        eN = findViewById(R.id.editNumber);
         Button envoye = (Button) findViewById(R.id.Envoyer);
-        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                frequence = newVal;
-            }
-        });
+
+
 
         envoye.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                nouveau_moduleManager.addNouveau_module(new Nouveau_module(frequence, eT.getText().toString()));
+                nouveau_moduleManager.addNouveau_module(new Nouveau_module(Integer.parseInt(eN.getText().toString()), eT.getText().toString()));
                 Nouveau_ModuleActivity.this.finish();
             }
         });
--- app/src/main/java/com/example/quentindoucet/perfhealth/vue/UpdateModuleActivituy.java
+++ app/src/main/java/com/example/quentindoucet/perfhealth/vue/UpdateModuleActivituy.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.NumberPicker;
 
 import com.example.quentindoucet.perfhealth.R;
 import com.example.quentindoucet.perfhealth.controleur.Nouveau_ModuleManager;
@@ -24,33 +23,30 @@ public class UpdateModuleActivituy extends AppCompatActivity{
     private int frequence_update;
     private String nom_update;
     private int position_update;
+    private EditText eN;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nouveau_module);
         eT = findViewById(R.id.nommodule);
+        eN = findViewById(R.id.editNumber);
         nouveau_moduleManager = new Nouveau_ModuleManager();
-        final NumberPicker numberPicker = (NumberPicker) findViewById(R.id.numberPicker);
         frequence_update = getIntent().getIntExtra("frequence", 0);
         nom_update = getIntent().getStringExtra("nom");
         position_update = getIntent().getIntExtra("position", 0);
-        numberPicker.setValue(frequence_update);
+        eN.setText(""+frequence_update);
         eT.setText(nom_update);
 
 
         Button envoye = (Button) findViewById(R.id.Envoyer);
-        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                frequence = newVal;
-            }
-        });
+
+
 
         envoye.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                nouveau_moduleManager.updateNouveau_module(position_update, new Nouveau_module(frequence, eT.getText().toString()));
+                nouveau_moduleManager.updateNouveau_module(position_update, new Nouveau_module(Integer.parseInt(eN.getText().toString()), eT.getText().toString()));
                 UpdateModuleActivituy.this.finish();
             }
         });
